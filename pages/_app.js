f101e5534f1d45b390f4abceadd20e0422879290
Diego10Rocha <diego93rocha@gmail.com>
2021-07-17 00:35:35 -0300

    inicio do alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,31 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AluraCommons'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+  #__next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
@@ -13,6 +33,7 @@ const theme = {
     primary: '#0070f3',
   },
 }
+
 
 export default function App({ Component, pageProps }) {
   return (
